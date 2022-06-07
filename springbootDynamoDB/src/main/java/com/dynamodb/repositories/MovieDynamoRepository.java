4da2a9a0a46af3b2dcd38b4e01546b629075f4b6
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-06-07 19:12:51 -0300

    Fix indentation.

--- springbootDynamoDB/src/main/java/com/dynamodb/repositories/MovieDynamoRepository.java
+++ springbootDynamoDB/src/main/java/com/dynamodb/repositories/MovieDynamoRepository.java
@@ -18,30 +18,28 @@ import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 
 @Repository
 public class MovieDynamoRepository {
-	
+
 	@Autowired
 	private AmazonDynamoDB client;
-	
+
 	public void findByKey(String key) {
 //		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
 //				.withRegion(Regions.US_WEST_2).build();
-				DynamoDB dynamoDB = new DynamoDB(client);
+		DynamoDB dynamoDB = new DynamoDB(client);
 
-				Table table = dynamoDB.getTable("movie");
+		Table table = dynamoDB.getTable("movie");
 
-				QuerySpec spec = new QuerySpec()
-				    .withKeyConditionExpression("FilmId = :v_id")
-				    .withValueMap(new ValueMap()
-				        .withString(":v_id", key));
+		QuerySpec spec = new QuerySpec().withKeyConditionExpression("FilmId = :v_id")
+				.withValueMap(new ValueMap().withString(":v_id", key));
 
-				ItemCollection<QueryOutcome> items = table.query(spec);
+		ItemCollection<QueryOutcome> items = table.query(spec);
 
-				Iterator<Item> iterator = items.iterator();
-				Item item = null;
-				while (iterator.hasNext()) {
-				    item = iterator.next();
-				    System.out.println(item.toJSONPretty());
-				}
+		Iterator<Item> iterator = items.iterator();
+		Item item = null;
+		while (iterator.hasNext()) {
+			item = iterator.next();
+			System.out.println(item.toJSONPretty());
+		}
 
 	}
 
