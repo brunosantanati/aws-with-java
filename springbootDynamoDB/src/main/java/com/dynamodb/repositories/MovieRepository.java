ee158faccfa71f569b90882906630a63fb70c25c
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-06-06 23:47:50 -0300

    Config, repository and application.yml.

---
+++ springbootDynamoDB/src/main/java/com/dynamodb/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.dynamodb.repositories;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface MovieRepository extends CrudRepository<Object, String> {
+
+}
