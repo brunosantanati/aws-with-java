7c9a215bf1d19b6e10daaa9b835304208069aaa5
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-09-07 21:00:16 -0300

    Links for migration from AWS SDK 1.x to 2.x

--- springbootDynamoDB-sdk-v2/src/main/java/com/dynamodb/controller/MusicController.java
+++ springbootDynamoDB-sdk-v2/src/main/java/com/dynamodb/controller/MusicController.java
@@ -12,6 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 //In the case of the music table, I've created a table with a partition key (pk), a sort key (sk) and a global secondary index (gsi1pk and gsi1sk).
 
+/*
+Migrating from version 1.x to 2.x of the AWS SDK for Java
+https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/migration.html
+
+What's different between the AWS SDK for Java 1.x and 2.x
+https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/migration-whats-different.html
+*/
+
 @RestController
 public class MusicController {
 	
