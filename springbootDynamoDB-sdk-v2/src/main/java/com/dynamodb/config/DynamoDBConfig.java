e6efd7a86040a932fad34895fee327e47220e210
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-09-09 10:21:00 -0300

    Set synchronous client configuration for 2.x

--- springbootDynamoDB-sdk-v2/src/main/java/com/dynamodb/config/DynamoDBConfig.java
+++ springbootDynamoDB-sdk-v2/src/main/java/com/dynamodb/config/DynamoDBConfig.java
@@ -6,8 +6,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
+import software.amazon.awssdk.http.apache.ProxyConfiguration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import java.time.Duration;
 
 @Configuration
 @EnableDynamoDBRepositories(basePackages = "com.dynamodb.repositories")
@@ -18,12 +23,27 @@ public class DynamoDBConfig {
                                          @Value("${aws.secret-key}") String secretKey
 
     ) {
+
+        ProxyConfiguration.Builder proxyConfig = ProxyConfiguration.builder();
+
+        ApacheHttpClient.Builder httpClientBuilder =
+                ApacheHttpClient.builder()
+                        .proxyConfiguration(proxyConfig.build())
+                        .maxConnections(10)
+                        .connectionTimeout(Duration.ofMillis(1000))
+                        .socketTimeout(Duration.ofMillis(1000));
+
+        ClientOverrideConfiguration.Builder overrideConfig =
+                ClientOverrideConfiguration.builder();
+
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(
                 accessKey,
                 secretKey);
 
         DynamoDbClient dynamoDbClient = DynamoDbClient.builder()
                 .region(Region.US_EAST_2)
+                .httpClientBuilder(httpClientBuilder)
+                .overrideConfiguration(overrideConfig.build())
                 .credentialsProvider(StaticCredentialsProvider.create(awsCredentials))
                 .build();
 
